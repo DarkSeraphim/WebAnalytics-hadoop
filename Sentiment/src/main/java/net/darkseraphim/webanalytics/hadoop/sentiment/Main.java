bf40d1f1568229af4304da9cb816623a095f14d2
4ndr3wN <andrewnoorden@hotmail.com>
2015-12-17 20:53:53 +0100

    Simply, placing output TODO's in Sentiment
    Simply, placing output TODO's in Sentiment

--- Sentiment/src/main/java/net/darkseraphim/webanalytics/hadoop/sentiment/Main.java
+++ Sentiment/src/main/java/net/darkseraphim/webanalytics/hadoop/sentiment/Main.java
@@ -14,14 +14,15 @@ import java.util.Iterator;
 import java.util.StringTokenizer;
 
 /**
- * Sentiment analysis skeleton
- * TODO: replace TheOutputClass with desired output class
+ * Sentiment analysis skeleton TODO: replace TheOutputClass with desired output
+ * class
  */
 public class Main {
 
     private static final String SECOND_OUTPUT = "top10";
 
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Row, Text, TheOutputClass> {
+
         private static final SimpleDateFormat sdf = new SimpleDateFormat("");
         private Text tag = new Text();
 
@@ -29,20 +30,24 @@ public class Main {
             // TODO: convert Rows into TheOutputClass instances, call output.collect(key, value) to produce results
             String line = ""; // tweet context
             String dateTimeline = ""; //constructing a timeline           
-            String dateDay = (""+value.get(5).toString());
-            if (dateDay.substring(0, 9).equals("2013-12-31") || dateDay.substring(0, 9).equals("2014-01-01")  ) { //Restrict tweets by date range dec 2013 - jan 2014
+            String dateDay = ("" + value.get(5).toString());
+            if (dateDay.substring(0, 9).equals("2013-12-31") || dateDay.substring(0, 9).equals("2014-01-01")) { //Restrict tweets by date range dec 2013 - jan 2014
                 dateTimeline = dateDay.substring(5); //Timeline                
                 line = value.get(4).toString();
                 StringTokenizer stringTokenizer = new StringTokenizer(line);
                 while (stringTokenizer.hasMoreTokens()) {
                     String token = stringTokenizer.nextToken();
-                    if (token.startsWith(":)") || token.equals("happy")){
+                    if (token.startsWith(":)") || token.equals("happy")) {
                         //basic requirement @ execution 2
-                    } else if (token.startsWith(":(") || token.equals("sad")){
-                        //basic requirement @ execution 2                                               
-                    }                                                                           
-                }                                                            
-            }            
+
+                        //TODO: output var dateTimeline
+                    } else if (token.startsWith(":(") || token.equals("sad")) {
+                        //basic requirement @ execution 2   
+                        
+                        //TODO: output var dateTimeline
+                    }
+                }
+            }
         }
     }
 
